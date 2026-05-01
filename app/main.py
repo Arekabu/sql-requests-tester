6b0d525176e32ff158c7ae35a65f1240ef4a7e06
Aleksey Yakomazov <arekabu@yandex.com>
2026-05-02 01:11:23 +0300

    refactoring

--- app/main.py
+++ app/main.py
@@ -16,5 +16,4 @@ app.include_router(pages_router)
 
 if __name__ == "__main__":
     print("🚀 Запуск сервера на http://localhost:8000")
-    print("📝 Убедитесь, что PostgreSQL запущен")
     uvicorn.run(app, host=app_config.host, port=app_config.port)
